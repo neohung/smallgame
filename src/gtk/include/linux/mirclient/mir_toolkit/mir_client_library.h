11e82325af14a83a2906a8de9e728151cbe2cc44
neo <neo.hung@quantatw.com>
2015-04-15 18:41:58 +0800

    add gtk linux makefile

---
+++ src/gtk/include/linux/mirclient/mir_toolkit/mir_client_library.h
@@ -0,0 +1,25 @@
+/*
+ * Copyright © 2012-2014 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef MIR_CLIENT_LIBRARY_H
+#define MIR_CLIENT_LIBRARY_H
+
+#include <mir_toolkit/mir_connection.h>
+#include <mir_toolkit/mir_surface.h>
+#include <mir_toolkit/mir_wait.h>
+
+#endif /* MIR_CLIENT_LIBRARY_H */
